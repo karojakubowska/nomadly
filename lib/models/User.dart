a1676fa97563bc7bc59d79b64441d7e4606d778b
karojakubowska <65358541+karojakubowska@users.noreply.github.com>
2023-02-12 16:42:11 +0100

    updated name of the modelUser

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,7 +1,8 @@
-class User{
+class UserModel{
   String? name;
   String? email;
   String? accountType;
+  String? imageurl;
 
   Map<String,dynamic> toJson()=>{
     'Name': name,
@@ -9,10 +10,25 @@ class User{
     'AccountType':accountType,
     };
 
-User.fromJson(Map<String,dynamic> json)
+UserModel.fromJson(Map<String,dynamic> json)
   {
     name=json['Name'];
     email=json['Email'];
     accountType=json['AccountType'];
+    imageurl=json['imageurl'];
     }
+UserModel.fromSnapshot(snapshot)
+  : name=snapshot.data()['Name'],
+  email=snapshot.data()['Email'],
+  accountType=snapshot.data()['AccountType'],
+  imageurl=snapshot.data()['Imageurl']
+  ;
+UserModel(snapshot)
+  : name=snapshot.data()['Name'],
+  email=snapshot.data()['Email'],
+  accountType=snapshot.data()['AccountType'],
+  imageurl=snapshot.data()['Imageurl']
+  ;
+  
+
 }
