81f47c85a5113bdcfbf34e34140b7e6538b56be2
PaulinaTymoszuk <p.tymoszuk@op.pl>
2023-02-22 00:29:49 +0100

    Add to do view - Travel (Part 2)

--- lib/screens/todo_view.dart
+++ lib/screens/todo_view.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nomadly_app/utils/app_styles.dart';
 
-class ToDoListScreen extends StatelessWidget {
+class ToDoListScreen extends StatefulWidget {
   const ToDoListScreen({Key? key}) : super(key: key);
+
+  @override
+  _ToDoListScreenState createState() => _ToDoListScreenState();
+}
+
+class _ToDoListScreenState extends State<ToDoListScreen> {
+  List<String> _todoList = [];
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +32,7 @@ class ToDoListScreen extends StatelessWidget {
           },
         ),
         title: Text(
-          'Add To do List',
+          'To do list',
           textAlign: TextAlign.center,
           style: GoogleFonts.roboto(
               textStyle: TextStyle(
@@ -38,37 +52,84 @@ class ToDoListScreen extends StatelessWidget {
         ),
       ),
       floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 90.0),
+        padding: EdgeInsets.all(8.0),
         child: FloatingActionButton(
           child: Icon(Icons.add),
           backgroundColor: Colors.blue,
           onPressed: () {
-            // Navigator.push(context,
-            //     MaterialPageRoute(builder: ((context) => AddTravelView())));
+            setState(() {
+              _todoList.add('');
+            });
           },
         ),
       ),
-      body: Center(
-        child:Padding(
-          padding: EdgeInsets.all(15.0),
-          child: ElevatedButton.icon(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+        elevation: 0,
+        child: Container(
+          height: 56.0,
+          margin: EdgeInsets.all(15.0),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Add To do',
+              style: TextStyle(fontSize: 18.0),
+            ),
             style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(60),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-                backgroundColor:
-                const Color.fromARGB(255, 50, 134, 252)),
-            onPressed: () {
-              //addTravel(pickedFile);
-            },
-            icon: const Icon(Icons.lock_open, size: 0),
-            label: const Text('Add To do',
-                style: TextStyle(fontSize: 20)),
+              primary: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              minimumSize: Size(double.infinity, 56.0),
+              padding: EdgeInsets.all(16.0),
+            ),
           ),
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: ListView.builder(
+            itemCount: _todoList.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller:
+                            TextEditingController(text: _todoList[index]),
+                        decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.0)),
+                              borderSide: BorderSide(
+                                  width: 1,
+                                  color: Color.fromARGB(255, 217, 217, 217)),
+                            ),
+                            filled: true,
+                            fillColor: Color.fromARGB(255, 249, 250, 250)),
+                        onChanged: (text) {
+                          _todoList[index] = text;
+                        },
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        setState(() {
+                          _todoList.removeAt(index);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
-
