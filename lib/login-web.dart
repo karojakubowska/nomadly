78922bf990bb6765dfb05471fcb75bbcdc7983e2
PaulinaTymoszuk <p.tymoszuk@op.pl>
2023-06-12 19:14:13 +0200

    fix

--- lib/login-web.dart
+++ lib/login-web.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:nomadly_app/app-web.dart';
 import 'package:nomadly_app/home-web.dart';
 
 class LoginPage extends StatefulWidget {
